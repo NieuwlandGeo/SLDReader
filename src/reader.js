22bd168d2c6ebe623a7b77d7e16398f840f870a5
Allart Kooiman <akooiman@nieuwland.nl>
2017-06-16 13:32:32 +0200

    Create array of named layers

--- src/reader.js
+++ src/reader.js
@@ -1,10 +1,31 @@
 import sax from 'sax';
 
-var parser = sax.parser(true);
+var parser = sax.parser(true, {
+  normalize: true
+});
 
-var result = {};
 
 export function reader(sld) {
-  parser.write(sld);
+  var result = [];
+  var currentlayer = {};
+  var currentTag = null;
+  parser.onopentag = (tag) => {
+    tag.parent = currentTag;
+    currentTag = tag;
+  };
+  parser.ontext = (text) => {
+    if (currentTag && currentTag.parent && currentTag.parent.name == 'sld:NamedLayer' && currentTag.name == 'sld:Name') {
+      currentlayer.name = text;
+    }
+  };
+  parser.onclosetag = (name) => {
+    if (name === 'sld:NamedLayer') {
+      result.push(currentlayer);
+      currentlayer = {};
+    }
+    currentTag = currentTag.parent;
+  };
+
+  parser.write(sld).close();
   return result;
 }
