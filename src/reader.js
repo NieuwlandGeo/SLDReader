48bdf5aab27ef36eb2e4ada08c516cf6f1b87e37
Allart Kooiman <akooiman@nieuwland.nl>
2017-06-29 16:42:46 +0200

    Add sld top components to object

--- src/reader.js
+++ src/reader.js
@@ -1,34 +1,59 @@
 
 
-var result = {};
-
 var parsers = {
-  NamedLayer: (element) => {
+  NamedLayer: (element, obj) => {
     let layername = getText(element, 'sld:Name');
-    result[layername] = {};
+    obj[layername] = {
+      styles: []
+    };
+    readNode(element, obj[layername]);
+  },
+  UserStyle: (element, obj) => {
+    let style = {
+      name: getText(element, 'sld:Name'),
+      default: getBool(element, 'sld:IsDefault')
+    };
+    readNode(element, style);
+    obj.styles.push(style);
+  },
+  FeatureTypeStyle: (element, obj) => {
+    obj.featuretypestyle = {
+      rules: []
+    };
+    readNode(element, obj);
   }
 };
 
-function readNode(node) {
+function readNode(node, obj) {
   for (let n = node.firstElementChild; n; n = n.nextElementSibling) {
     if (parsers[n.localName]) {
-      parsers[n.localName](n);
+      parsers[n.localName](n, obj);
     }
   }
 }
 
 function getText(element, tagName) {
-  return element.getElementsByTagName(tagName).item(0).textContent;
+  const collection = element.getElementsByTagName(tagName);
+  return (collection.length) ? collection.item(0).textContent : '';
+}
+
+function getBool(element, tagName) {
+  const collection = element.getElementsByTagName(tagName);
+  if (collection.length) {
+    return (collection.item(0).textContent);
+  }
+  return false;
 }
 
 
 export function reader(sld) {
+  var result = {};
   var parser = new DOMParser();
   var doc = parser.parseFromString(sld, 'application/xml');
 
   for (let n = doc.firstChild; n; n = n.nextSibling) {
     result.version = n.getAttribute('version');
-    readNode(n);
+    readNode(n, result);
   }
   return result;
 }
--- test/reader.test.js
+++ test/reader.test.js
@@ -17,6 +17,14 @@ describe('Reads xml', function() {
     for (let s of layernames) {
       expect(result[s]).to.be.an.instanceof(Object);
     }
-
+  });
+  it('has style for waterbodies', function() {
+    const wbstyles = result['WaterBodies'].styles;
+    expect(wbstyles).to.be.an.instanceof(Array);
+    expect(wbstyles).to.have.length(12);
+  });
+  it('default has featuretypestyle', function() {
+    const style = result['WaterBodies'].styles['0'];
+    expect(style.featuretypestyle).to.be.an.instanceof(Object);
   });
 });
