2ea4e39ffbe35a3f14c34f1728c50feb9dbdf2d6
allart <allartk@gmail.com>
2017-07-05 11:54:58 +0200

    Use specific layer from sld

--- src/Style.js
+++ src/Style.js
@@ -7,11 +7,31 @@ class Style {
   /**
    * Read xml file
    * @param  {string} sld xml string
-   * @param {string} [layername] Select layer matching layername, defaults to first layer
+   * @param {string} [layername] Select layer matching lowercased layername, defaults to first layer
    * @return {void}
    */
-  read(sld) {
+  read(sld, layername) {
     this.sld = Reader(sld);
+    if (layername) {
+      this.layer = this.sld.layer.filter(l => {
+        if (layername) {
+          return (l.name.toLowerCase() == layername);
+        }
+      })['0'];
+    } else {
+      this.layer = this.sld.layer['0'];
+    }
+  }
+  /**
+   * Change selected layer to style
+   * @param {string} layername  Select layer matching lowercased layername
+   */
+  setLayer(layername) {
+    this.layer = this.sld.layer.filter(l => {
+      if (layername) {
+        return (l.name.toLowerCase() == layername);
+      }
+    })['0'];
   }
 
 
