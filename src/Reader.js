8bcc39197bc57d42c67eaf10630b9e9dcaf09e08
Allart Kooiman <akooiman@nieuwland.nl>
2018-03-15 11:57:19 +0100

    doc function in reader

--- src/Reader.js
+++ src/Reader.js
@@ -1,5 +1,6 @@
 /**
  * Generic parser for elements with maxOccurs > 1
+ * it pushes result of readNode(node) to array on obj[prop]
  * @private
  * @param {Element} node the xml element to parse
  * @param {object} obj  the object to modify
@@ -14,7 +15,8 @@ function addPropArray(node, obj, prop) {
 }
 
 /**
- * Generic parser fir maxOccurs = 1
+ * Generic parser for maxOccurs = 1
+ * it sets result of readNode(node) to array on obj[prop]
  * @private
  * @param {Element} node the xml element to parse
  * @param {object} obj  the object to modify
@@ -28,7 +30,7 @@ function addProp(node, obj, prop) {
 
 function getText(element, tagName) {
   const collection = element.getElementsByTagName(tagName);
-  return (collection.length) ? collection.item(0).textContent : '';
+  return collection.length ? collection.item(0).textContent : '';
 }
 
 function getBool(element, tagName) {
@@ -118,6 +120,13 @@ const parsers = {
   },
 };
 
+/**
+ * walks over xml nodes
+ * @private
+ * @param  {Element} node derived from xml
+ * @param  {object} obj recieves results
+ * @return {void}
+ */
 function readNode(node, obj) {
   for (let n = node.firstElementChild; n; n = n.nextElementSibling) {
     if (parsers[n.localName]) {
@@ -125,7 +134,6 @@ function readNode(node, obj) {
     }
   }
 }
-
 
 /**
  * Creates a object from an sld xml string, for internal usage
@@ -144,7 +152,6 @@ export default function Reader(sld) {
   return result;
 }
 
-
 /**
  * @typedef StyledLayerDescriptor
  * @name StyledLayerDescriptor
@@ -154,73 +161,70 @@ export default function Reader(sld) {
  */
 
 /**
-* @typedef Layer
-* @name Layer
-* @description a typedef for Layer, the actual style object for a single layer
-* @property {string} name layer name
-* @property {Object[]} styles See explanation at [Geoserver docs](http://docs.geoserver.org/stable/en/user/styling/sld/reference/styles.html)
-* @property {Boolean} styles[].default
-* @property {FeatureTypeStyle[]} styles[].featuretypestyles
-*/
+ * @typedef Layer
+ * @name Layer
+ * @description a typedef for Layer, the actual style object for a single layer
+ * @property {string} name layer name
+ * @property {Object[]} styles See explanation at [Geoserver docs](http://docs.geoserver.org/stable/en/user/styling/sld/reference/styles.html)
+ * @property {Boolean} styles[].default
+ * @property {FeatureTypeStyle[]} styles[].featuretypestyles
+ */
 
 /**
-* @typedef FeatureTypeStyle
-* @name FeatureTypeStyle
-* @description a typedef for FeatureTypeStyle: {@link http://schemas.opengis.net/se/1.1.0/FeatureStyle.xsd xsd}
-* @property {Rule[]} rules
-*/
-
-
-/**
-* @typedef Rule
-* @name Rule
-* @description a typedef for Rule to match a feature: {@link http://schemas.opengis.net/se/1.1.0/FeatureStyle.xsd xsd}
-* @property {string} name rule name
-* @property {Filter} [filter]
-* @property {boolean} [elsefilter]
-* @property {integer} [minscaledenominator]
-* @property {integer} [maxscaledenominator]
-* @property {PolygonSymbolizer} [polygonsymbolizer]
-* @property {LineSymbolizer}  [linesymbolizer]
-* @property {PointSymbolizer} [pointsymbolizer]
-* */
+ * @typedef FeatureTypeStyle
+ * @name FeatureTypeStyle
+ * @description a typedef for FeatureTypeStyle: {@link http://schemas.opengis.net/se/1.1.0/FeatureStyle.xsd xsd}
+ * @property {Rule[]} rules
+ */
 
 /**
-* @typedef Filter
-* @name Filter
-* @description [ogc filters]( http://schemas.opengis.net/filter/1.1.0/filter.xsd) should have only one prop
-* @property {array} [featureid] filter
-* @property {object} [or]  filter
-* @property {object} [and]  filter
-* @property {object} [not]  filter
-* @property {array} [propertyisequalto]  filter
-* */
-
-
-/**
-* @typedef PolygonSymbolizer
-* @name PolygonSymbolizer
-* @description a typedef for [PolygonSymbolizer](http://schemas.opengis.net/se/1.1.0/Symbolizer.xsd)
-* @property {Object} fill
-* @property {array} fill.css
-* @property {Object} stroke
-* @property {array} stroke.css
-* */
+ * @typedef Rule
+ * @name Rule
+ * @description a typedef for Rule to match a feature: {@link http://schemas.opengis.net/se/1.1.0/FeatureStyle.xsd xsd}
+ * @property {string} name rule name
+ * @property {Filter} [filter]
+ * @property {boolean} [elsefilter]
+ * @property {integer} [minscaledenominator]
+ * @property {integer} [maxscaledenominator]
+ * @property {PolygonSymbolizer} [polygonsymbolizer]
+ * @property {LineSymbolizer}  [linesymbolizer]
+ * @property {PointSymbolizer} [pointsymbolizer]
+ * */
 
 /**
-* @typedef LineSymbolizer
-* @name LineSymbolizer
-* @description a typedef for [LineSymbolizer](http://schemas.opengis.net/se/1.1.0/Symbolizer.xsd)
-* @property {Object} stroke
-* @property {array} stroke.css
-* */
-
+ * @typedef Filter
+ * @name Filter
+ * @description [ogc filters]( http://schemas.opengis.net/filter/1.1.0/filter.xsd) should have only one prop
+ * @property {array} [featureid] filter
+ * @property {object} [or]  filter
+ * @property {object} [and]  filter
+ * @property {object} [not]  filter
+ * @property {array} [propertyisequalto]  filter
+ * */
 
 /**
-* @typedef PointSymbolizer
-* @name PointSymbolizer
-* @description a typedef for [PointSymbolizer](http://schemas.opengis.net/se/1.1.0/Symbolizer.xsd)
-* @property {Object} graphic
-* @property {Object} graphic.externalgraphic
-* @property {string} graphic.externalgraphic.onlineresource
-* */
+ * @typedef PolygonSymbolizer
+ * @name PolygonSymbolizer
+ * @description a typedef for [PolygonSymbolizer](http://schemas.opengis.net/se/1.1.0/Symbolizer.xsd)
+ * @property {Object} fill
+ * @property {array} fill.css
+ * @property {Object} stroke
+ * @property {array} stroke.css
+ * */
+
+/**
+ * @typedef LineSymbolizer
+ * @name LineSymbolizer
+ * @description a typedef for [LineSymbolizer](http://schemas.opengis.net/se/1.1.0/Symbolizer.xsd)
+ * @property {Object} stroke
+ * @property {array} stroke.css
+ * */
+
+/**
+ * @typedef PointSymbolizer
+ * @name PointSymbolizer
+ * @description a typedef for [PointSymbolizer](http://schemas.opengis.net/se/1.1.0/Symbolizer.xsd)
+ * @property {Object} graphic
+ * @property {Object} graphic.externalgraphic
+ * @property {string} graphic.externalgraphic.onlineresource
+ * */
