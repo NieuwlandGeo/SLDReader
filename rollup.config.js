5f3730c7de4a9bce67be97cffe6a03805df857ea
allart <allartk@gmail.com>
2018-03-23 17:13:57 +0100

    hello rollup

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import buble from 'rollup-plugin-buble';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/sldreader.js',
+    format: 'umd',
+    name: 'sldreader',
+  },
+  plugins: [buble()],
+};
