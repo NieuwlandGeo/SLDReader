662ad691e7f62678c5e214793fdf86da37298c5c
Allart Kooiman <akooiman@nieuwland.nl>
2018-11-22 16:31:40 +0100

    create example with live edit

--- docs/assets/grenzen.js
+++ docs/assets/grenzen.js
@@ -1,4 +1,29 @@
-/* global ol SLDReader */
+/* global ol SLDReader CodeMirror */
+// the xml editor
+const editor = CodeMirror.fromTextArea(document.getElementById('sld'), {
+  lineNumbers: true,
+  lineWrapping: true,
+  mode: 'xml',
+});
+
+/**
+ * @param {object} vector layer
+ * @param {string} text the xml text
+ * @param {number} pointresolution the projected resolution
+ * apply sld
+ */
+function applySLD(vector, text, pointresolution) {
+  const sldObject = SLDReader.Reader(text);
+  const sldLayer = SLDReader.getLayer(sldObject, 'bestuurlijkegrenzen:provincies');
+  const style = SLDReader.getStyle(sldLayer, 'bestuurlijkegrenzen:provincies');
+  const format = new ol.format.GeoJSON();
+  vector.setStyle(feature => {
+    const geojson = JSON.parse(format.writeFeature(feature));
+    const rules = SLDReader.getRules(style.featuretypestyles['0'], geojson, pointresolution);
+    return SLDReader.OlStyler(SLDReader.getGeometryStyles(rules), geojson.geometry.type);
+  });
+}
+
 const vectorSource = new ol.source.Vector({
   format: new ol.format.GeoJSON(),
   url(extent) {
@@ -36,21 +61,19 @@ const map = new ol.Map({
 });
 map.addControl(new ol.control.MousePosition());
 
+const pointresolution = ol.proj.getPointResolution(
+  map.getView().getProjection(),
+  map.getView().getResolution(),
+  map.getView().getCenter()
+);
+
 fetch('assets/sld-provincies.xml')
   .then(response => response.text())
-  .then(text => {
-    const sldObject = SLDReader.Reader(text);
-    const sldLayer = SLDReader.getLayer(sldObject, 'bestuurlijkegrenzen:provincies');
-    const style = SLDReader.getStyle(sldLayer, 'bestuurlijkegrenzen:provincies');
-    const format = new ol.format.GeoJSON();
-    const pointresolution = ol.proj.getPointResolution(
-      map.getView().getProjection(),
-      map.getView().getResolution(),
-      map.getView().getCenter()
-    );
-    vector.setStyle(feature => {
-      const geojson = JSON.parse(format.writeFeature(feature));
-      const rules = SLDReader.getRules(style.featuretypestyles['0'], geojson, pointresolution);
-      return SLDReader.OlStyler(SLDReader.getGeometryStyles(rules), geojson.geometry.type);
-    });
-  });
+  .then(text => editor.setValue(text));
+
+/**
+ * update map if sld is edited
+ */
+editor.on('change', cm => {
+  applySLD(vector, cm.getValue(), pointresolution);
+});
