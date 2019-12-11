dd19d161c0e4d408dcfc5058d90a353d794f8143
jkonieczny <razi91@o2.pl>
2019-12-11 13:18:01 +0100

    octagon

--- src/OlStyler.js
+++ src/OlStyler.js
@@ -303,6 +303,20 @@ function pointStyle(pointsymbolizer) {
               }),
           }),
         });
+      case 'octagon':
+        return new Style({
+          image: new RegularShape({
+            fill,
+            points: 8,
+            radius1: radius,
+            stroke:
+              stroke ||
+              new Stroke({
+                color: fillColor,
+                width: radius / 2,
+              }),
+          }),
+        });
       case 'x':
         return new Style({
           image: new RegularShape({
