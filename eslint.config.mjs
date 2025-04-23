839120a8df9e30c4c844562f37dc2aaa2aee9604
AD\akopinga <akopinga@nieuwland.nl>
2025-04-23 13:07:13 +0200

    ESLint ignore transpiled test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 
 export default defineConfig([
+  globalIgnores(["test/transpiled"]),
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
